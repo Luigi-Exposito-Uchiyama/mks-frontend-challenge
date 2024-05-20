4765e798ae588d75c4e56ea9ae8f6cb83ab3dbf8
Luigi Uchiyama <luigi_uchiyama@outlook.com>
2024-05-20 00:06:24 -0300

    Footer Adicionado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import Header from "../components/header/page";
 import Store from "../components/store/page";
+import Footer from "../components/footer/page";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Store />
+      <Footer />
     </>
   );
 }
---
+++ src/components/footer/page.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+const Footer = () => {
+  return (
+    <S.Container>
+      <S.Footer>MKS sistemas © Todos os direitos reservados</S.Footer>
+    </S.Container>
+  );
+};
+
+export default Footer;
